e63d0d836ca229e3f468bc20b06727d0d0d43553
Xing Zhou <zhouxingapply@gmail.com>
2017-01-10 15:06:47 +0800

    Add Microsoft Translator

---
+++ MicrosoftTranslator/MicrosoftTranslator/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization.Json;
+using System.Runtime.Serialization;
+using System.Web;
+
+namespace MicrosoftTranslator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AdmAccessToken admToken;
+            string headerValue;
+
+            AdmAuthentication admAuth = new AdmAuthentication();
+
+            try
+            {
+                admToken = admAuth.GetAccessToken();
+                // Create a header with the access_token property of the returned token
+                headerValue = "Bearer " + admToken.access_token;
+                TranslateMethod(headerValue);
+            }
+            catch (WebException e)
+            {
+                // ProcessWebException(e);
+                Console.WriteLine("Press any key to continue...");
+                Console.ReadKey(true);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine("Press any key to continue...");
+                Console.ReadKey(true);
+            }
+        }
+
+        private static void TranslateMethod(string authToken)
+        {
+            string text = "Use pixels to express measurements for padding and margins.";
+            string from = "en";
+            string to = "de";
+            string uri = "http://api.microsofttranslator.com/v2/Http.svc/Translate?text=" + HttpUtility.UrlEncode(text) + "&from=" + from + "&to=" + to;
+            HttpWebRequest httpWebRequest = (HttpWebRequest)WebRequest.Create(uri);
+            httpWebRequest.Headers.Add("Authorization", authToken);
+            WebResponse response = null;
+            try
+            {
+                response = httpWebRequest.GetResponse();
+                using (Stream stream = response.GetResponseStream())
+                {
+                    DataContractSerializer dcs = new DataContractSerializer(Type.GetType("System.String"));
+                    string translation = (string)dcs.ReadObject(stream);
+                    Console.WriteLine("Translation for source text '{0}' from {1} to {2} is", text, "en", "de");
+                    Console.WriteLine(translation);
+                }
+                Console.WriteLine("Press any key to continue...");
+                Console.ReadKey(true);
+            }
+            catch
+            {
+                throw;
+            }
+            finally
+            {
+                if (response != null)
+                {
+                    response.Close();
+                    response = null;
+                }
+            }
+        }
+
+        private static void ProcessWebException(WebException e)
+        {
+            Console.WriteLine("{0}", e.ToString());
+            // Obtain detailed error information
+            string strResponse = string.Empty;
+            using (HttpWebResponse response = (HttpWebResponse)e.Response)
+            {
+                using (Stream responseStream = response.GetResponseStream())
+                {
+                    using (StreamReader sr = new StreamReader(responseStream, System.Text.Encoding.ASCII))
+                    {
+                        strResponse = sr.ReadToEnd();
+                    }
+                }
+            }
+            Console.WriteLine("Http status code={0}, error message={1}", e.Status, strResponse);
+        }
+    }
+}
